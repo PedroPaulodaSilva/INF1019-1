1b4d940181eca933330b79ba0b4e602ad70df691
STEPHANIE MAGALHAES FAY <c1721631@iracema.grad.inf.puc-rio.br>
2019-05-21 15:32:34 -0300

    Final touches

--- T1/02/Program.c
+++ T1/02/Program.c
@@ -9,12 +9,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-	printf("\n>>>>>> Program initialized (pid = %d)\n", getpid());
+	printf("\n>>>>>> Programa %s iniciado (pid = %d)\n", argv[0], getpid());
 	char * testvar = strdup(argv[2]), * pch;
-	printf("DEBUG: %s\n", testvar);
-	printf("%s %s %s\n", argv[0], argv[1], argv[2]);
 	int fatherPid = strtol(argv[1], NULL, 10), timeSlice;
-	printf("DEBUG: %d %d\n", fatherPid, timeSlice);
 	pch = strtok (testvar, ",");
 	while (pch != NULL) {
 		timeSlice = atoi(pch);		
@@ -25,13 +22,13 @@ int main(int argc, char *argv[]){
 		pch = strtok (NULL, ",");
 
 		if (pch != NULL) {
-			printf("\n>>>>>> Entering IO\n");
+			printf("\n>>>>>> Pausa para I/O\n");
 			kill(fatherPid, SIGUSR1);
 			kill(getpid(), SIGSTOP);
 		}
 	}
 
-	printf("\n>>> Process %d - will terminate\n", getpid());
+	printf("\n>>> Processo %d finalizado \n", getpid());
 	kill(fatherPid, SIGCHLD);
 
     return 0;
